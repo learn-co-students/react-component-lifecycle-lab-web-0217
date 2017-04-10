0126b837eb3e14491dd074299a1149822c742274
fattyduck <dison.ruan@yahoo.com>
2017-04-10 09:48:08 -0400

    Done.

--- components/TweetWall.js
+++ components/TweetWall.js
@@ -10,10 +10,22 @@ export default class TweetWall extends React.Component {
   }
 
   // TODO: componentWillMount()
-
-  // TODO: shouldComponentUpdate()
+  componentWillMount(){
+    this.setState({
+      tweets: this.props.newTweets
+    })
+  }
+  // TODO:
+  shouldComponentUpdate(nextProps, nextState){
+    this.props.tweets !== nextProps.tweets
+  }
 
   // TODO: componentWillReceiveProps()
+  componentWillReceiveProps(nextProps){
+    this.setState({
+      tweets: this.state.tweets.concat(nextProps.newTweets).reverse()
+    })
+  }
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => {
